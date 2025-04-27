969e761c57701d78d5d1a6ddee7f49ebb17914df
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-27 11:16:33 +0700

    feat:import user data method and get user data

--- eda/__init__.py
+++ eda/__init__.py
@@ -1,4 +1,7 @@
-from eda.user_data import load_user_data
+from eda.user_data import load_user_data, get_head_user_datas
 
 if __name__ == '__main__':
-    print(load_user_data())
+    path = '../ml-100k/u.user'
+    cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+    print(load_user_data(path, cols))
+    get_head_user_datas(path, cols)
