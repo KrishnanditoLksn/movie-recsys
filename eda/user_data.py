f93c6f2fd859e0cf788444b5ab5dadaa16d77cfc
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-27 11:13:26 +0700

    feat:load user data from csv and get top 5

--- eda/user_data.py
+++ eda/user_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 
-def load_user_data():
-    user_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-    users = pd.read_csv('../ml-100k/u.user', sep='|', names=user_cols, encoding='latin-1')
+def load_user_data(path, cols):
+    users = pd.read_csv(path, names=cols, sep='|', encoding="utf-8")
     return users
+
+
+def get_head_user_datas(path, cols):
+    top_five = pd.read_csv(path, names=cols, sep='|', encoding="utf-8")
+    print("TOP 5 DATAS :  ", top_five.head(5))
+
