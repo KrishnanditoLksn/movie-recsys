bf7c13ba9ac5089c654d351165f2b0ee2c6e78f0
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-27 12:52:48 +0700

    delete: init files

--- eda/__init__.py
+++
@@ -1,7 +0,0 @@
-from eda.user_data import load_user_data, get_head_user_datas
-
-if __name__ == '__main__':
-    path = '../ml-100k/u.user'
-    cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-    print(load_user_data(path, cols))
-    get_head_user_datas(path, cols)
